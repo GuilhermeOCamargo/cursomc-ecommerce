4d50ee73b4f88ce8e9c4e6b3bc6a3436cf4a4bed
Guilherme Camargo <guilherme.ocamargo@gmail.com>
2018-11-11 03:28:52 -0200

    Criação da classe de domínio e Repository de Produtos

--- src/main/java/com/ecommerce/cursomc/CursomcApplication.java
+++ src/main/java/com/ecommerce/cursomc/CursomcApplication.java
@@ -8,12 +8,16 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.ecommerce.cursomc.domain.Categoria;
+import com.ecommerce.cursomc.domain.Produto;
 import com.ecommerce.cursomc.repositories.CategoriaRepository;
+import com.ecommerce.cursomc.repositories.ProdutoRepository;
 
 @SpringBootApplication
 public class CursomcApplication implements CommandLineRunner {
 	@Autowired
 	private CategoriaRepository categoriaRepository;
+	@Autowired
+	private ProdutoRepository produtoRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -21,8 +25,23 @@ public class CursomcApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
+		/*Categoria*/
 		Categoria cat1 = new Categoria(null, "Informática");
 		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		/*Produto*/
+		Produto p1 = new Produto(null, "Computador", 2000.00);
+		Produto p2 = new Produto(null, "Impressora", 800.00);
+		Produto p3 = new Produto(null, "Mouse", 80.00);
+		//Relacionando Categorias e Produtos
+		cat1.getProdutos().addAll(Arrays.asList(p1, p2, p3));
+		cat2.getProdutos().addAll(Arrays.asList(p2));
+		p1.getCategorias().addAll(Arrays.asList(cat1));
+		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
+		p3.getCategorias().addAll(Arrays.asList(cat1));
+		
+		/*Repository*/
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
 	}
 }
